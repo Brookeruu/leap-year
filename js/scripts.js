1047a2f42e902906e1d2ce95e47ea90939922d91
Brooke Perry and Christopher Cahill <christopher@>
2018-10-24 10:37:46 -0700

    create function and it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 
+function isItLeapYear(year) {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return (year + " is a leap year");
+  }
+  else if (year % 100 === 0 && year % 400 === 0) {
+    return (year + " is a leap year");
+  }
+  else {return (year + " is not a leap year");}
+}
 
 $(document).ready(function() {
-  $("").submit
-
+  $("#leapYearForm").submit(function(event) {
+    event.preventDefault();
+    var inputYear = parseInt($("#leapYearInput").val());
+    var result = isItLeapYear(inputYear);
+    $("#leapYearResult").text(result);
+    console.log(result);
+  });
 });
